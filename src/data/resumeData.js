7d641dac33ee3e0461fa96b00e8968eed73d40d8
yyijoo <yyijoo@gmail.com>
2019-03-26 11:47:56 +0900

    Resume : Add resume json data

--- src/data/resumeData.js
+++ src/data/resumeData.js
@@ -1,5 +1,5 @@
-export default [
-  {
+export default {
+  techSkill: {
     category: "Technical Skills",
     contents: [
       {
@@ -22,19 +22,114 @@ export default [
       },
     ],
   },
-  {
+  projects: {
     category: "Projects",
     contents: [
       {
-        itemName: "Experienced",
+        project: "Codestates Admin",
+        role: "Front-end Engineer",
+        term: "2019.02 - 2019.03",
+        desc:
+          "통합 수강생 관리, 웹사이트 콘텐츠 관리, 결제 관리를 위한 관리자 페이지",
         item: [
-          "Javascript",
-          "HTML/CSS",
-          "Redux",
-          "styled-componenets",
-          "eslint",
+          "템플릿 기반 웹앱 기획 및 클라이언트 구조 설계",
+          "수강생 정보 CRUD 인터페이스 및 REST API 구현",
+        ],
+      },
+      {
+        project: "Codestates Website",
+        role: "Front-end",
+        term: "2019.02 - 2019.03",
+        desc: "코스 안내, 신청, 결제 및 상담이 이뤄지는 공식 웹사이트",
+        item: [
+          "React, Redux, styled - component를 활용한 통합 수강생 관리 페이지 개발",
+          "수강신청, 코스소개, 메인 페이지 UI 개편",
+          "브라우저 사이즈별 반응형 대응",
+          "hotfix 대응 등 유지보수",
+        ],
+      },
+      {
+        project: "Jobstates",
+        role: "Front-end Engineer",
+        term: "2018.12 - 2018.12",
+        desc:
+          "통합 수강생 관리, 웹사이트 콘텐츠 관리, 결제 관리를 위한 관리자 페이지",
+        item: [
+          "React & Redux를 사용한 웹앱 구조 설계",
+          "전체 레이아웃 설계 및 구현",
+          "이력서 페이지 구현",
+          "Google vision API를 이용한 이미지 텍스트 추출 기능 구현",
+          "AWS S3 클라이언트 앱 배포",
+        ],
+      },
+      {
+        project: "My-Log",
+        role: "Front-end & Back-end Engineer",
+        term: "2018.11 - 2018.11",
+        desc: "사진 및 텍스트로 일상을 기록 및 관리하는 온라인 일기 서비스",
+        item: [
+          "Express & Sequelize로 CRUD",
+          "AWS S3 이미지 업로드 기능",
+          "AWS EC2 서버 앱 배포",
         ],
       },
     ],
   },
-]
+  experience: {
+    category: "Experience",
+    contents: [
+      {
+        experience: "코드스테이츠, 소프트웨어 엔지니어",
+        term: "2019.01 - 2019.03 / 3개월",
+        item: [
+          "코드스테이츠 공식 웹사이트 및 관리자 사이트 개발",
+          "코드스테이츠 프리 및 이머시브 코스 교육 지원",
+        ],
+      },
+      {
+        experience: "카카오모빌리티 사업기획팀",
+        term: "2017.08 - 2018.05 / 10개월",
+        item: [
+          "카카오드라이버 담당. 공급자 확장 전략 수립 및 실행",
+          "신규 온라인 신차 판매 플랫폼 담당.초기 서비스 및 BM 설계와 신규 제휴사 발굴",
+        ],
+      },
+      {
+        experience: "카카오 파킹TF → 사업기획팀",
+        term: "2016.03 - 2017.08 / 1년 6개월",
+        item: [
+          "신규 온라인 신차 판매 플랫폼 담당. 초기 서비스 설계 및 제휴 계약",
+          "택시 시승 서비스 담당.제휴사 발굴 및 타겟팅 시승 상품 개발",
+          "파킹TF 담당.초기 BM 설계와 공급자 확장 전략 수립 및 실행",
+        ],
+      },
+      {
+        experience: "파킹스퀘어 전략기획팀",
+        term: "2014.04 - 2016.03 / 2년",
+        item: [
+          "대외 관계 전략 수립 및 실행",
+          "유저 확장 전략 수립 및 실행",
+          "핵심지표 설정 및 관리",
+        ],
+      },
+    ],
+  },
+  education: {
+    category: "Education",
+    contents: [
+      {
+        education:
+          "Code States, Advanced Software Engineering Immersive Program",
+        term: "2018.10 시작 - 2018.12 종료",
+      },
+      {
+        education: "성균관대학교 사회학 학사",
+        term: "2011.03 편입 - 2014-03 수료 - 2016.02 졸업",
+      },
+      {
+        education: "한국외국어대학교 글로벌캠퍼스 영어통번역학 학사",
+        term: "2008.03 입학 - 2010.06 중퇴",
+      },
+    ],
+  },
+}
